0d996dfd093f9394f3f862d6f9107f8d333c70fa
hanze <303709827@qq.com>
2019-11-25 13:30:52 +0800

    add encryption

--- src/main/java/hanze/lab/springcloud/ClientApplication.java
+++ src/main/java/hanze/lab/springcloud/ClientApplication.java
@@ -3,6 +3,7 @@ package hanze.lab.springcloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ClientApplication {
 
 	@Value("${user.role}")
 	private String role;
+
+	@Value("${user.password}")
+	private String password;
+
+	@Value("${user.email}")
+	private String email;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
@@ -25,6 +33,7 @@ public class ClientApplication {
 			method = RequestMethod.GET,
 			produces = MediaType.TEXT_PLAIN_VALUE)
 	public String whoami(@PathVariable("username") String username) {
-		return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
+		return String.format("Hello! You're %s and you'll become a(n) %s only if your password is %s \n email: %s", username, role, password,email
+		);
 	}
 }
